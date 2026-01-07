58c86cff2156784af5d801d892d0ae9e176280dc
jeisoncaguana <jeisoncaguana@gmail.com>
2026-01-07 10:53:14 +0100

    new: casco de moto

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(expressLayouts);
 app.set("layout", "layout"); // usa views/layout.ejs
 
 app.get("/", (_req, res) => {
-  res.render("index", { title: "Pao" });
+  res.render("index", { title: "Pao ❤️" });
 });
 
 app.listen(PORT, () => console.log(`✅ http://localhost:${PORT}`));
